3da629a2e9412ad3a5a196ee4f9a5b1410df9851
Jalson1982 <35011526+Jalson1982@users.noreply.github.com>
2020-03-01 12:42:08 +0100

    Update RNShopifyPackage.java

--- android/src/main/java/com/reactnativeshopify/RNShopifyPackage.java
+++ android/src/main/java/com/reactnativeshopify/RNShopifyPackage.java
@@ -1,27 +1,27 @@
 package com.reactnativeshopify;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import android.util.Log;
 
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
 
-public class RNShopifyPackage implements ReactPackage {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class ShopifyPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNShopifyModule(reactContext));
-    }
-    
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new ShopifyModule(reactContext));
+        return modules;
     }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-      return Collections.emptyList();
+        return Collections.emptyList();
     }
 }
